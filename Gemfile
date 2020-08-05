46acb45d28e2a6c226d1985ae3a73801b28a46b4
nakagawahiroki <iwbb1022@icloud.com>
2020-08-06 00:23:12 +0900

    create user

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby '2.6.1'
 
 gem 'bootstrap', '~> 4.1.1'
 gem 'jquery-rails'
-
+gem 'rails-controller-testing'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 # Use postgresql as the database for Active Record
---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+
+    get root_path
+    assert_template 'home/top'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', about_path
+  end
+end
