00d438aa64e272298c5cb7a50074e94a30f2cf25
nakagawahiroki <iwbb1022@icloud.com>
2020-08-06 01:11:52 +0900

    create validate function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+  validates :name, presence: true, length: {maximum: 50}
+
+  validates :email, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,32 @@ class UserTest < ActiveSupport::TestCase
   test "shoule be valid" do
     assert @user.valid?
   end
+
+  test "name should be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@icloudddd.com"
+    assert_not @user.valid?
+  end
+
+  test "email should be unique" do
+    user_dup = @user.dup #dupは、同じ属性を持つデータを複製するためのメソッドです。
+    @user.save
+    user_dup.email = @user.email.upcase
+    assert_not user_dup.valid?
+  end
+
 end
