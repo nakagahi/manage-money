604fd0b8cf74a55cfc7a580e3c91cc98a59f5e83
nakagawahiroki <iwbb1022@icloud.com>
2020-08-07 12:30:59 +0900

    created signup function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(usr_params)
+
+    if @user.save
+      flash[:success] = "#{@user.name}さんを登録しました。"
+      redirect_to @user
+    else
+      flash.now[:danger] = "失敗しました。。"
+      render 'new'
+    end
+  end
+
+  private
+
+  def usr_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
