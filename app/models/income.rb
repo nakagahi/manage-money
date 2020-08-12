96a3bb295fc4006d0f390f0ee9b3ccda106ca899
nakagawahiroki <iwbb1022@icloud.com>
2020-08-12 21:46:26 +0900

    createvalidate

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,3 +1,8 @@
 class Income < ApplicationRecord
+
+  validates :money, presence: true
+  validates :user_id, presence: true
+  validates :month, presence: true
+  validates :type, presence: true
+
 end
-  
