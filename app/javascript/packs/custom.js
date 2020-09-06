4732874e73a1ec349e6df6cf3e10afac5d043ef5
nakagawahiroki <iwbb1022@icloud.com>
2020-09-06 17:05:01 +0900

    トップページに動きをつけた

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,1 +1,26 @@
 
+$(function(){
+    $(window).scroll(function (){
+        $('.possible').each(function(){
+            var position = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 200){
+              $(this).addClass('active');
+            }
+        });
+    });
+
+    $(window).scroll(function (){
+        $('.wish').each(function(){
+            var position = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 200){
+              $(this).addClass('active');
+            }
+        });
+    });
+
+
+});
